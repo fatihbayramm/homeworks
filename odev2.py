ccdef22bcccdb1ab81d4c6f526e8193f57f4599b
Fatih Bayram <fatihbayramybs@gmail.com>
2022-02-19 01:29:06 +0300

    odev2

---
+++ odev2.py
@@ -0,0 +1,33 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+    
+    def sound(self):
+        raise NotImplementedError()
+
+class Cat(Animal):
+    def sound(self):
+        print("meow meow !")
+
+
+class Cow(Animal):
+    def sound(self):
+        print("mooo !")
+
+
+class Dog(Animal):
+    def sound(self):
+        print("hav hav !")
+
+
+
+
+c = Cat("animal")
+c.sound()
+cw = Cow("animal")
+cw.sound()
+d = Dog("animal")
+d.sound()
+
+
+
